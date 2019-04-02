a5c7d85dacae8fc6d002f8c11e4b96e4fb475dfd
wilkersoh <plgu@hotmail.com>
2019-04-02 21:55:06 +0800

    url changed

--- src/app.js
+++ src/app.js
@@ -51,12 +51,12 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, {latitude, longtitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if(error) {
             return res.send({ error})
         }
 
-        forecast(latitude, longtitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return res.send({ error})
             }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,12 +2,12 @@ const request = require('request')
 
 const geocode = (address, callback) => {
   
-    const url = 'https://api.mapbox.com/v4/mapbox.emerald/'+ address +'?access_token=pk.eyJ1IjoieXpkZXYiLCJhIjoiY2p0enB3dDAwMm9iejN6cW5kbjFkcTNodyJ9.uZKJN23voJOcAHfgLAScyQ'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoieXpkZXYiLCJhIjoiY2p0enB3dDAwMm9iejN6cW5kbjFkcTNodyJ9.uZKJN23voJOcAHfgLAScyQ'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (body.message || body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
